aff2f28d0c586982c2f6389ca947e6e5f1d070b6
Melvin <melvingm101@gmail.com>
2019-10-01 19:56:09 +0400

    Created Frynapse Repository

---
+++ public/js/website.js
@@ -0,0 +1,198 @@
+hashCode = function(s){
+  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+}
+
+function ques1(){
+    console.log("This works!");
+    var answer = document.getElementById("ques1");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -1033060309){
+        window.location.href = "107944096.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques2(){
+    console.log("This works!");
+    var answer = document.getElementById("ques2");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -849452327){
+        window.location.href = "107944097.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques3(){
+    console.log("This works!");
+    var answer = document.getElementById("ques3");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -861391249){
+        window.location.href = "948700210.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques4(){
+    console.log("This works!");
+    var answer = document.getElementById("ques4");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == 121820797){
+        window.location.href = "948700179.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques5(){
+    console.log("This works!");
+    var answer = document.getElementById("ques5");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == 1864941562){
+        window.location.href = "948700148.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques6(){
+    console.log("This works!");
+    var answer = document.getElementById("ques6");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -1301811418 || hashCode(answer1.toLowerCase()) == -971402416 || hashCode(answer1.toLowerCase()) == 651378865){
+        window.location.href = "948700117.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques7(){
+    console.log("This works!");
+    var answer = document.getElementById("ques7");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -1407655532 || hashCode(answer1.toLowerCase()) == 1910618513 || hashCode(answer1.toLowerCase()) == 2097563856){
+        window.location.href = "948700086.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques8(){
+    console.log("This works!");
+    var answer = document.getElementById("ques8");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -678105702 || hashCode(answer1.toLowerCase()) == 788546331 || hashCode(answer1.toLowerCase()) == 195717470){
+        window.location.href = "948700055.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques9(){
+    console.log("This works!");
+    var answer = document.getElementById("ques9");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -427098976){
+        window.location.href = "655061727.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques10(){
+    console.log("This works!");
+    var answer = document.getElementById("ques10");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -868118479){
+        window.location.href = "655061758.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques11(){
+    console.log("This works!");
+    var answer = document.getElementById("ques11");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -1332418844){
+        window.location.href = "655061789.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques12(){
+    console.log("This works!");
+    var answer = document.getElementById("ques12");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -833068232 || hashCode(answer1.toLowerCase()) == -532523681 || hashCode(answer1.toLowerCase()) == -984301440){
+        window.location.href = "655061820.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques13(){
+    console.log("This works!");
+    var answer = document.getElementById("ques13");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == -848672652 || hashCode(answer1.toLowerCase()) == -1412788717){
+        window.location.href = "655061851.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques14(){
+    console.log("This works!");
+    var answer = document.getElementById("ques14");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == 94833107){
+        window.location.href = "948700298.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
+
+function ques15(){
+    console.log("This works!");
+    var answer = document.getElementById("ques15");
+    var answer1 = answer.value + "";
+    if (hashCode(answer1.toLowerCase()) == 96632902){
+        window.location.href = "thanks.html";
+    }
+    
+    else{
+        alert("The answer is wrong. Try again!!");
+    }
+}
